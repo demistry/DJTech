ff42b570d0088e28bd17f9e08d3c24d5c18c777b
demistry <dave11ilen@gmail.com>
2017-07-23 22:10:03 +0100

    team members layout designed

---
+++ app/src/main/java/com/kryptkode/cyberman/djtech/TeamMembersActivity.java
@@ -0,0 +1,13 @@
+package com.kryptkode.cyberman.djtech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeamMembersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.team_members_layout);
+    }
+}
